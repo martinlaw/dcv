ef12bf7c9384f7a4e46f73cb3239149614d5cb69
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2022-03-08 14:51:24 +0000

    delete scratch code

--- R/scratch.R
+++ R/scratch.R
@@ -1,22 +1,2 @@
 library(metafor)
-dat <- escalc(measure="RR",
-              ai=c(3, 5, 2, 0, 1),
-              bi=c(32, 39, 43, 15, 24),
-              ci=c(6, 7, 4, 1, 2),
-              di=c(29, 35, 41, 14, 23))
-
-# Check:
-exp(dat$yi) # agrees with draft RRs
-
-# NOTE: Yao et al. has N=50 but draft states N=60.
-
-# FE model:
-rma(yi, vi, data=dat, method="FE") # NOTE: different results to draft
-# RE model (Hartung-Knapp / Sidik-Jonkman):
-rma(yi, vi, data=dat, method="SJ") # NOTE: different results to draft
-rma(yi, vi, data=dat, method="REML") # NOTE: different results to draft
-
-usethis::use_vignette("meta")
-
-
 
