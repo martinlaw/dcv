cf8f3e99b5cfc6d17afff04b8826f12e4b55ab35
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2022-03-11 15:35:39 +0000

    Added atelectasis meta-analysis

--- R/scratch.R
+++ R/scratch.R
@@ -1,2 +1,16 @@
-library(metafor)
-
+# library(metafor)
+# ?escalc
+# dat.resp <- escalc(measure="MD",
+#               m1i=c(31.74, 29.8, 27.7, 23, 38.6, 23.6),
+#               sd1i=c(2.71, 4.57, 6.13, 3.06, 6.2, 5.8),
+#               m2i=c(27, 20.53, 26.3, 19.9, 29.6, 17.3),
+#               sd2i=c(2.43, 2.61, 6.13, 1.73, 3.2, 4.6),
+#               n1i=c(25, 15, 45, 44),
+#               n2i=c(25, 15, 45, 42),
+#               slab=c("Yao et al, 2020",
+#                      "Ammar et al, 2021",
+#                      "Li et al (OLA), 2020",
+#                      "Li et al (Standard), 2020",
+#                      "Xukai et al, 2021",
+#                      "Sahutoglu et al, 2020")
+#               )
